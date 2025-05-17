0f2d7786a825759981798defd29f827103313f3e
Niklas Rolin <niklas.rolinv@gmail.com>
2025-05-17 17:14:53 +0200

    fix(env): improve game over conditions and reward handling

--- gym/env.py
+++ gym/env.py
@@ -74,158 +74,160 @@ class BattlesnakeEnv:
             raise ValueError(f"Expected {self.n_snakes} actions, got {len(actions)}")
         self.turns += 1
         rewards = [0.0] * self.n_snakes
-        done = False
-
-        next_heads = []
-        moved_snakes = [False] * self.n_snakes
-
-        tails_moving_away = set()
-        for i in range(self.n_snakes):
-            if not self.snake_alive[i]:
-                continue
-            snake = self.snakes[i]
-            if len(snake) > 1:
-                growing = (len(snake) < self.target_lengths[i])
-                if not growing:
-                    tails_moving_away.add(snake[-1])
-
-        for i in range(self.n_snakes):
-            if not self.snake_alive[i]:
-                next_heads.append(None)
-                continue
-            snake = self.snakes[i]
-            head_x, head_y = snake[0]
-            dx, dy = self.DIRECTIONS[actions[i]]
-            new_head = (head_x + dx, head_y + dy)
-            if len(snake) > 1 and snake[1] == new_head:
-                dx, dy = self.DIRECTIONS[(actions[i] + 2) % 4]
-                new_head = (head_x + dx, head_y + dy)
-            next_heads.append(new_head)
-            moved_snakes[i] = True
-
-        for i in range(self.n_snakes):
-            if not self.snake_alive[i] or next_heads[i] is None:
-                continue
-            new_head = next_heads[i]
-            if not (0 <= new_head[0] < self.board_size and 0 <= new_head[1] < self.board_size):
-                self.snake_alive[i] = False
-                self.elimination_causes[i] = "wall_collision"
-                rewards[i] = -1.0
-                next_heads[i] = None
-                for x, y in self.snakes[i]:
-                    if 0 <= x < self.board_size and 0 <= y < self.board_size:
-                        self.board[y, x] = 0
-
-        head_positions = {}
-        for i in range(self.n_snakes):
-            if not self.snake_alive[i] or next_heads[i] is None:
-                continue
-            x, y = next_heads[i]
-            if (x, y) in head_positions:
-                head_positions[(x, y)].append(i)
-            else:
-                head_positions[(x, y)] = [i]
-            if 0 <= x < self.board_size and 0 <= y < self.board_size and self.board[y, x] >= 2:
-                collided_snake_id = self.board[y, x] - 2
-                if (x, y) in tails_moving_away:
-                    pass
-                elif collided_snake_id == i:
-                    self.snake_alive[i] = False
-                    self.elimination_causes[i] = "self_collision"
-                    rewards[i] = -1.0
-                    next_heads[i] = None
-                    for sx, sy in self.snakes[i]:
-                        if 0 <= sx < self.board_size and 0 <= sy < self.board_size:
-                            self.board[sy, sx] = 0
-                else:
-                    self.snake_alive[i] = False
-                    self.elimination_causes[i] = "body_collision"
-                    rewards[i] = -1.0
-                    next_heads[i] = None
-                    for sx, sy in self.snakes[i]:
-                        if 0 <= sx < self.board_size and 0 <= sy < self.board_size:
-                            self.board[sy, sx] = 0
-
-        for pos, snake_ids in head_positions.items():
-            if len(snake_ids) <= 1:
-                continue
-            lengths = [(len(self.snakes[i]), i) for i in snake_ids if self.snake_alive[i]]
-            if not lengths:
-                continue
-            lengths.sort(reverse=True)
-            longest_length = lengths[0][0]
-            winners = [l[1] for l in lengths if l[0] == longest_length]
-            if len(winners) == len(snake_ids):
-                for i in snake_ids:
-                    if self.snake_alive[i]:
-                        self.snake_alive[i] = False
-                        self.elimination_causes[i] = "head_to_head"
-                        rewards[i] = -1.0
-                        next_heads[i] = None
-                        for x, y in self.snakes[i]:
-                            if 0 <= x < self.board_size and 0 <= y < self.board_size:
-                                self.board[y, x] = 0
-            else:
-                for i in snake_ids:
-                    if i not in winners and self.snake_alive[i]:
-                        self.snake_alive[i] = False
-                        self.elimination_causes[i] = "head_to_head"
-                        rewards[i] = -1.0
-                        next_heads[i] = None
-                        for x, y in self.snakes[i]:
-                            if 0 <= x < self.board_size and 0 <= y < self.board_size:
-                                self.board[y, x] = 0
-
-        self.food_eaten = [False] * self.n_snakes
-        for i in range(self.n_snakes):
-            if not self.snake_alive[i] or next_heads[i] is None:
-                continue
-            x, y = next_heads[i]
-            if 0 <= x < self.board_size and 0 <= y < self.board_size and self.board[y, x] == 1:
-                self.food_eaten[i] = True
-                self.snake_health[i] = 100
-                rewards[i] = 0.01
-                self.food.remove((x, y))
-                self.board[y, x] = 0
-                self.target_lengths[i] += 1
-
-        for i in range(self.n_snakes):
-            if not self.snake_alive[i] or next_heads[i] is None:
-                continue
-            snake = self.snakes[i]
-            x, y = next_heads[i]
-            snake.insert(0, (x, y))
-            if 0 <= x < self.board_size and 0 <= y < self.board_size:
-                self.board[y, x] = 2 + i
-            current_length = len(snake)
-            target_length = self.target_lengths[i]
-            if current_length > target_length:
-                tail_x, tail_y = snake.pop()
-                if 0 <= tail_x < self.board_size and 0 <= tail_y < self.board_size:
-                    self.board[tail_y, tail_x] = 0
-
-        for i in range(self.n_snakes):
-            if self.snake_alive[i] and not self.food_eaten[i]:
-                self.snake_health[i] -= 1
-                if self.snake_health[i] <= 0:
-                    self.snake_alive[i] = False
-                    self.elimination_causes[i] = "starvation"
-                    rewards[i] = -1.0
-                    for x, y in self.snakes[i]:
-                        if 0 <= x < self.board_size and 0 <= y < self.board_size:
-                            self.board[y, x] = 0
-
-        self.spawn_food()
-
-        #done = all(not alive for alive in self.snake_alive)
+        # done = all(not alive for alive in self.snake_alive)
         # When we train we dont want to continue when our snake dies
         done = False
         if not self.snake_alive[0]:
             done = True
+            rewards[0] = -1.0
         # If we win we are also done
         if not self.snake_alive[2] and not self.snake_alive[3]:
             done = True
-            rewards[0] = 1
+            rewards[0] = 1.0
+        if not done:
+            next_heads = []
+            moved_snakes = [False] * self.n_snakes
+
+            tails_moving_away = set()
+            for i in range(self.n_snakes):
+                if not self.snake_alive[i]:
+                    continue
+                snake = self.snakes[i]
+                if len(snake) > 1:
+                    growing = (len(snake) < self.target_lengths[i])
+                    if not growing:
+                        tails_moving_away.add(snake[-1])
+
+            for i in range(self.n_snakes):
+                if not self.snake_alive[i]:
+                    next_heads.append(None)
+                    continue
+                snake = self.snakes[i]
+                head_x, head_y = snake[0]
+                dx, dy = self.DIRECTIONS[actions[i]]
+                new_head = (head_x + dx, head_y + dy)
+                if len(snake) > 1 and snake[1] == new_head:
+                    dx, dy = self.DIRECTIONS[(actions[i] + 2) % 4]
+                    new_head = (head_x + dx, head_y + dy)
+                next_heads.append(new_head)
+                moved_snakes[i] = True
+
+            for i in range(self.n_snakes):
+                if not self.snake_alive[i] or next_heads[i] is None:
+                    continue
+                new_head = next_heads[i]
+                if not (0 <= new_head[0] < self.board_size and 0 <= new_head[1] < self.board_size):
+                    self.snake_alive[i] = False
+                    self.elimination_causes[i] = "wall_collision"
+                    rewards[i] = -1.0
+                    next_heads[i] = None
+                    for x, y in self.snakes[i]:
+                        if 0 <= x < self.board_size and 0 <= y < self.board_size:
+                            self.board[y, x] = 0
+
+            head_positions = {}
+            for i in range(self.n_snakes):
+                if not self.snake_alive[i] or next_heads[i] is None:
+                    continue
+                x, y = next_heads[i]
+                if (x, y) in head_positions:
+                    head_positions[(x, y)].append(i)
+                else:
+                    head_positions[(x, y)] = [i]
+                if 0 <= x < self.board_size and 0 <= y < self.board_size and self.board[y, x] >= 2:
+                    collided_snake_id = self.board[y, x] - 2
+                    if (x, y) in tails_moving_away:
+                        pass
+                    elif collided_snake_id == i:
+                        self.snake_alive[i] = False
+                        self.elimination_causes[i] = "self_collision"
+                        rewards[i] = -1.0
+                        next_heads[i] = None
+                        for sx, sy in self.snakes[i]:
+                            if 0 <= sx < self.board_size and 0 <= sy < self.board_size:
+                                self.board[sy, sx] = 0
+                    else:
+                        self.snake_alive[i] = False
+                        self.elimination_causes[i] = "body_collision"
+                        rewards[i] = -1.0
+                        next_heads[i] = None
+                        for sx, sy in self.snakes[i]:
+                            if 0 <= sx < self.board_size and 0 <= sy < self.board_size:
+                                self.board[sy, sx] = 0
+
+            for pos, snake_ids in head_positions.items():
+                if len(snake_ids) <= 1:
+                    continue
+                lengths = [(len(self.snakes[i]), i) for i in snake_ids if self.snake_alive[i]]
+                if not lengths:
+                    continue
+                lengths.sort(reverse=True)
+                longest_length = lengths[0][0]
+                winners = [l[1] for l in lengths if l[0] == longest_length]
+                if len(winners) == len(snake_ids):
+                    for i in snake_ids:
+                        if self.snake_alive[i]:
+                            self.snake_alive[i] = False
+                            self.elimination_causes[i] = "head_to_head"
+                            rewards[i] = -1.0
+                            next_heads[i] = None
+                            for x, y in self.snakes[i]:
+                                if 0 <= x < self.board_size and 0 <= y < self.board_size:
+                                    self.board[y, x] = 0
+                else:
+                    for i in snake_ids:
+                        if i not in winners and self.snake_alive[i]:
+                            self.snake_alive[i] = False
+                            self.elimination_causes[i] = "head_to_head"
+                            rewards[i] = -1.0
+                            next_heads[i] = None
+                            for x, y in self.snakes[i]:
+                                if 0 <= x < self.board_size and 0 <= y < self.board_size:
+                                    self.board[y, x] = 0
+
+            self.food_eaten = [False] * self.n_snakes
+            for i in range(self.n_snakes):
+                if not self.snake_alive[i] or next_heads[i] is None:
+                    continue
+                x, y = next_heads[i]
+                if 0 <= x < self.board_size and 0 <= y < self.board_size and self.board[y, x] == 1:
+                    self.food_eaten[i] = True
+                    self.snake_health[i] = 100
+                    rewards[i] = 0.01
+                    self.food.remove((x, y))
+                    self.board[y, x] = 0
+                    self.target_lengths[i] += 1
+
+            for i in range(self.n_snakes):
+                if not self.snake_alive[i] or next_heads[i] is None:
+                    continue
+                snake = self.snakes[i]
+                x, y = next_heads[i]
+                snake.insert(0, (x, y))
+                if 0 <= x < self.board_size and 0 <= y < self.board_size:
+                    self.board[y, x] = 2 + i
+                current_length = len(snake)
+                target_length = self.target_lengths[i]
+                if current_length > target_length:
+                    tail_x, tail_y = snake.pop()
+                    if 0 <= tail_x < self.board_size and 0 <= tail_y < self.board_size:
+                        self.board[tail_y, tail_x] = 0
+
+            for i in range(self.n_snakes):
+                if self.snake_alive[i] and not self.food_eaten[i]:
+                    self.snake_health[i] -= 1
+                    if self.snake_health[i] <= 0:
+                        self.snake_alive[i] = False
+                        self.elimination_causes[i] = "starvation"
+                        rewards[i] = -1.0
+                        for x, y in self.snakes[i]:
+                            if 0 <= x < self.board_size and 0 <= y < self.board_size:
+                                self.board[y, x] = 0
+
+            self.spawn_food()
+
+            if not self.snake_alive[2] and not self.snake_alive[3]:
+                rewards[0] = 1
 
 
         info = {
